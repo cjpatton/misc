144172b9196654ce1b58530633539ddb17fcb4e9
Christopher Patton <chrispatton@gmail.com>
2018-02-20 12:03:11 -0500

    Having un.

---
+++ interview/21Feb2018/col.go
@@ -0,0 +1,40 @@
+package col
+
+import (
+	"math"
+)
+
+// Given a sequence of integers X and input y, find index i that minimizes
+// abs(X[i] - y).
+
+type Strings struct {
+	idx map[int]string
+	lst []int
+}
+
+func NewStrings() *Strings {
+	st := new(Strings)
+	st.idx = make(map[int]string)
+	st.lst = make([]int, 0)
+	return st
+}
+
+func (st *Strings) Insert(s string, n int) {
+	st.idx[len(st.lst)] = s
+	st.lst = append(st.lst, n)
+}
+
+func (st *Strings) NearestTo(n int) (s string) {
+	if len(st.lst) == 0 {
+		return ""
+	}
+	i := 0
+	m := st.lst[0]
+	for j, x := range st.lst {
+		if math.Abs(float64(x-n)) < math.Abs(float64(m-n)) {
+			i = j
+			m = x
+		}
+	}
+	return st.idx[i]
+}
---
+++ interview/21Feb2018/col_test.go
@@ -0,0 +1,37 @@
+package col
+
+import (
+	"crypto/rand"
+	"testing"
+)
+
+func score(s string) int {
+	var d []byte
+	if len(s) < 3 {
+		d = append([]byte(s), make([]byte, 3-len(s))...)
+	} else {
+		d = []byte(s)[:3]
+	}
+	return int(d[0])<<24 | int(d[1])<<16 | int(d[2])<<8
+}
+
+func TestInsertsNearest(t *testing.T) {
+	total := 20
+	length := 10
+
+	st := NewStrings()
+	buf := make([]byte, length)
+	for i := 0; i < total; i++ {
+		if _, err := rand.Read(buf); err != nil {
+			t.Fatal("rand.Read() fails:", err)
+		}
+		for j, x := range buf {
+			buf[j] = byte((int(x) % 25) + 97) // Not uniform
+		}
+		s := string(buf)
+		st.Insert(s, score(s))
+		t.Logf("%s", s)
+	}
+
+	t.Logf("nearest: %s", st.NearestTo(score("hella")))
+}
