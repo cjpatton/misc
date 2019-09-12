8b9e700ea9de6a9f02ca7cdbd8ee5362c64e4abb
--global <--global>
2019-09-12 17:49:21 -0400

    Do some excercises

--- interview/17Sep2019/binary.py
+++ interview/17Sep2019/binary.py
@@ -1,10 +1,97 @@
-# How is a binary search tree implemented?
-# How do you perform preorder traversal in a given binary tree?
-# How do you traverse a given binary tree in preorder without recursion?
-# How do you perform an inorder traversal in a given binary tree?
-# How do you print all nodes of a given binary tree using inorder traversal without recursion?
+import random
 # How do you implement a postorder traversal algorithm?
 # How do you traverse a binary tree in postorder traversal without recursion?
 # How are all leaves of a binary search tree printed?
 # How do you count a number of leaf nodes in a given binary tree?
 # How do you perform a binary search in a given array?
+
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+    self.visited = False
+
+  def insert(self, val):
+    if val < self.val:
+      if self.left != None:
+        self.left.insert(val)
+      else:
+        self.left = Node(val)
+    else:
+      if self.right != None:
+        self.right.insert(val)
+      else:
+        self.right = Node(val)
+
+  def find(self, val):
+    if val == self.val:
+      return True
+    elif val < self.val and self.left != None:
+      return self.left.find(val)
+    elif self.right != None:
+      return self.right.find(val)
+    return False
+
+  def preordered(self, array):
+    array.append(self.val)
+    if self.left != None:
+      self.left.preordered(array)
+    if self.right != None:
+      self.right.preordered(array)
+
+  def preordered_it(self, array):
+    stack = [self]
+    while len(stack) > 0:
+      node = stack.pop()
+      array.append(node.val)
+      if node.right != None:
+        stack.append(node.right)
+      if node.left != None:
+        stack.append(node.left)
+
+  def inordered(self, array):
+    if self.left != None:
+      self.left.inordered(array)
+    array.append(self.val)
+    if self.right != None:
+      self.right.inordered(array)
+
+  def inordered_it(self, array):
+    stack = [self]
+    while len(stack) > 0:
+      top = stack.pop()
+      if top.visited:
+        array.append(top.val)
+      else:
+        top.visited = True
+        if top.left == None and top.right == None:
+          stack.append(top)
+        else:
+          if top.right != None:
+            stack.append(top.right)
+          stack.append(top)
+          if top.left != None:
+            stack.append(top.left)
+
+if __name__ == '__main__':
+  tree = Node(random.randint(0,100))
+  for _ in range(100):
+    tree.insert(random.randint(0,100))
+
+  # P1 How do you traverse a given binary tree in preorder without recursion?
+  expected = []
+  tree.preordered(expected)
+  got = []
+  tree.preordered_it(got)
+  print 'p1', 'pass' if got == expected else 'fail'
+
+  # P2 How do you print all nodes of a given binary tree using inorder traversal
+  # without recursion?
+  expected = []
+  tree.inordered(expected)
+  got = []
+  tree.inordered_it(got)
+  print 'p2', 'pass' if got == expected else 'fail'
+
--- interview/17Sep2019/linked.py
+++ interview/17Sep2019/linked.py
@@ -1,8 +1,4 @@
 import random
-# How do you find the middle element of a singly linked list in one pass?
-# How do you check if a given linked list contains a cycle? How do you find the starting node of the cycle?
-# How do you reverse a linked list?
-# How do you reverse a singly linked list without recursion?
 # How are duplicate nodes removed in an unsorted linked list?
 # How do you find the length of a singly linked list?
 # How do you find the third node from the end in a singly linked list?
@@ -33,16 +29,95 @@ def print_linked(linked):
     print curr.val
     curr = curr.child
 
+def linked_to_array(linked):
+  array = []
+  curr = linked
+  while curr != None:
+    array.append(curr.val)
+    curr = curr.child
+  return array
+
 # P1 How do you find the middle element of a singly linked list in one pass?
 #
 # This is easy if I can store O(n) data or do recursion. What data are stored?
 # Is it arbitrary?
+#
+# TODO Can we do this without O(n) space?
 def p1(s):
   length = random.randint(0,100)
-  return length / 2 == s(make_linked([0 for i in range(length)]))
+  data = [ random.randint(0,100) for _ in range(length) ]
+  return data[length/2] == s(make_linked(data))
 
 def p1s1(linked):
-  return 23
+  links = []
+  curr = linked
+  while curr != None:
+    links.append(curr)
+    curr = curr.child
+  return links[len(links)/2].val
+
+
+# P2 How do you check if a given linked list contains a cycle? How do you find
+# the starting node of the cycle?
+#
+# I take this to mean that some value repeats.
+def p2(s):
+  data = list(range(100))
+  i = random.randint(0,99)
+  j = random.randint(0,99)
+  random.shuffle(data)
+  data[i] = data[j]
+  return data[j] == s(make_linked(data))
+
+def p2s1(linked):
+  rep = {}
+  curr = linked
+  while curr != None:
+    if rep.get(curr.val) != None:
+      return curr.val
+    rep[curr.val] = True
+    curr = curr.child
+  return None
+
+# P3 How do you reverse a singly linked list without recursion?
+def p3(s):
+  data = list(range(100))
+  return list(reversed(data)) == linked_to_array(s(make_linked(data)))
+
+def p3s1(linked):
+  head = linked
+  prv = None
+  while head != None:
+    nxt = head.child
+    head.child = prv
+    prv = head
+    head = nxt
+  return prv
+
+# P4 How are duplicate nodes removed in an unsorted linked list?
+#
+# Is it doubly or singly linked?
+def p4(s):
+  data = [ random.randint(0,10) for _ in range(100) ]
+  return list(set(data)) == list(sorted(linked_to_array(s(make_linked(data)))))
+
+
+def p4s1(linked):
+  unique_vals = set()
+  prv = None
+  head = linked
+  while head != None:
+    val = head.val
+    if val in unique_vals:
+      prv.child = head.child
+    else:
+      prv = head
+    unique_vals.add(val)
+    head = head.child
+  return linked
 
 if __name__ == '__main__':
   print 'p1', 'pass' if p1(p1s1) else 'fail'
+  print 'p2', 'pass' if p2(p2s1) else 'fail'
+  print 'p3', 'pass' if p3(p3s1) else 'fail'
+  print 'p4', 'pass' if p4(p4s1) else 'fail'
