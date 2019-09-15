5e3248157d7e21cf78ed0e135c3d4db67d2b1f71
Christopher Patton <chrispatton@gmail.com>
2019-09-15 10:33:06 -0400

    LRU

--- interview/17Sep2019/misc.py
+++ interview/17Sep2019/misc.py
@@ -15,3 +15,43 @@ def genip(S, I):
 
 for ip in genip('11211', []):
   print ip
+
+class LRU:
+  def __init__(self, cap):
+    assert cap > 0
+    self.cap = cap
+    self.cache = {}
+    self.order = {}
+    self.queue = []
+
+  def _use(self, key):
+    del self.queue[self.order[key]]
+    self.queue.append(key)
+    self.order[key] = len(self.queue) - 1
+
+  def get(self, key):
+    val = self.cache.get(key)
+    if val != None:
+      self._use(key)
+    return val
+
+  def set(self, key, val):
+    if self.cache.get(key) != None: # Update key
+      self.cache[key] = val
+    else: # Insert new key
+      if len(self.queue) == self.cap: # Evict LRU element
+        evicted = self.queue[0]
+        del self.cache[evicted]
+        del self.order[evicted]
+        self.queue = self.queue[1:]
+      self.queue.append(key)
+      self.order[key] = len(self.queue) - 1
+      self.cache[key] = val
+
+lru = LRU(3)
+lru.set(1, 'hello')
+lru.set(2, 'go')
+lru.set(3, 'gators')
+print 'pass' if lru.get(1) == 'hello' else 'fail'
+lru.set(4, 'chomp')
+print 'pass' if lru.get(2) == None else 'fail'
